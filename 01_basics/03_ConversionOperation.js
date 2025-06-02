888b13d266ea46a32f45a9e1861eb58bda1ee653
TanuTechnical <tanulakhwani001@gmail.com>
2025-06-02 12:48:27 +0000

    conversionOfDataTypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,19 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valeInNumber);
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+let someNumber= 99
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
